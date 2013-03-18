ed56ccb08e03bc87b5c4cb3d958b4f0a6c3ab8fa
chrismo <chrismo@clabs.org>
2013-03-18 00:04:50 -0500

    Sand from above!!

---
+++ plugins/RubyBukkit/raining_sand.rb
@@ -0,0 +1,27 @@
+Plugin.is {
+  name 'RainingSand'
+  version '0.0.1'
+  author 'chrismo'
+}
+
+import 'org.bukkit.Material'
+import 'org.bukkit.Location'
+import 'org.bukkit.event.player.PlayerMoveEvent'
+
+class RainingSand < RubyPlugin
+  def onEnable
+    getServer.getOnlinePlayers.each do |player|
+      registerEvent(PlayerMoveEvent, :Normal) do |event|
+        rainOneSand(event.player)
+      end
+    end
+  end
+
+  def rainOneSand(player)
+    location = player.getLocation
+    world = location.getWorld
+    new_block_location = Location.new(world, location.getX, location.getY + 100, location.getZ)
+    new_block = world.getBlockAt(new_block_location)
+    new_block.setType(Material::SAND)
+  end
+end
